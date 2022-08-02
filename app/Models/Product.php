1b87217f03d26cb7c8356f28961b734bd8e9c47f
Rifang <rifang.asmara12@gmail.com>
2022-08-02 12:28:55 +0700

    repairing setup

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,4 +9,5 @@ class Product extends Model
 {
     use HasFactory;
     protected $fillable = ['product_name', 'product_category', 'product_description', 'price'];
+    // fillable tidak diberlakukan pada stock tapi masih dapat terisi
 }
